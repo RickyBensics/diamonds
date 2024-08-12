5ba2d0add1b7d1d5e879ec24d129964b68718e64
Richard Bensics <rbensics@yahoo.com>
2024-08-12 10:50:46 -0700

    Initial commit

---
+++ diamonds.R
@@ -0,0 +1,8 @@
+# Plotting the diamonds dataset
+ggplot(diamonds, aes(x = carat, y = price, color = cut)) +
+  geom_point(alpha = 0.5) +
+  theme_minimal() +
+  labs(title = "Diamond Price vs. Carat",
+       x = "Carat",
+       y = "Price (USD)",
+       color = "Cut Quality")
