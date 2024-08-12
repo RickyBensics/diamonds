cbff0ed2bdf91d56d2277fa90fc3738d071b4031
Richard Bensics <rbensics@yahoo.com>
2024-08-12 10:53:10 -0700

    Initial commit

--- diamonds.R
+++ diamonds.R
@@ -1,3 +1,4 @@
+library("ggplot2")
 # Plotting the diamonds dataset
 ggplot(diamonds, aes(x = carat, y = price, color = cut)) +
   geom_point(alpha = 0.5) +
@@ -6,3 +7,5 @@ ggplot(diamonds, aes(x = carat, y = price, color = cut)) +
        x = "Carat",
        y = "Price (USD)",
        color = "Cut Quality")
+
+usethis::use_github_pages()
